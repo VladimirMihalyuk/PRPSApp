b0ba9ae24a3fcdd95bbb91470e68276884cf5a90
VladimirMihalyuk <37588348+VladimirMihalyuk@users.noreply.github.com>
2019-08-10 16:25:39 +0300

    Added bottom navigation

--- app/src/main/java/com/example/prpsapp/MainActivity.kt
+++ app/src/main/java/com/example/prpsapp/MainActivity.kt
@@ -2,11 +2,23 @@ package com.example.prpsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.example.prpsapp.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navController = this.findNavController(R.id.nav_host_fragment)
+
+        NavigationUI.setupWithNavController(navigation_view, navController);
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 }
